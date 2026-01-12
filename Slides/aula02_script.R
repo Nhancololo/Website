40bedc59ea03d7a3d8ebeff98eb2af88b7183643
Nhancololo <amnahncololo@gmail.com>
2026-01-12 13:36:03 -0300

    Built site for gh-pages

---
+++ Slides/aula02_script.R
@@ -0,0 +1,13 @@
+
+
+'---------------------------Script Aula 02--------------------------------
+
+Ops… pegadinha acadêmica 😄
+
+O script completo estará disponível após a segunda aula.
+
+Porém vc pode ler a seção 2.8 e 2.9 do material de didatico, disponível no 
+link: https://nhancololo.github.io/, a aba 
+Teaching, opção Spatial statistics.
+
+-------------------------------------------------------------------------'
