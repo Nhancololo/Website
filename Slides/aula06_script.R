70bed8718b86a5392bf10f2c779beb491f998892
Nhancololo <amnahncololo@gmail.com>
2026-02-05 13:12:45 -0300

    Built site for gh-pages

---
+++ Slides/aula06_script.R
@@ -0,0 +1,13 @@
+
+
+'---------------------------Script Aula 08--------------------------------
+
+                  Ops… pegadinha acadêmica 😄
+
+O script ainda não está disponível.
+
+Porém vc pode ler capítulo 5 do material de apoio, disponível no 
+link: https://nhancololo.github.io/, a aba 
+Teaching-> Spatial statistics ->Textbook.
+
+-------------------------------------------------------------------------'
